67af36b5e9acbc1c6b9b5959ec2bdbe1151fe070
Safa <haikadysafa@gmail.com>
2026-05-24 13:15:30 +0530

    Connected Railway database

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -2,21 +2,27 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
 
-    host: "localhost",
-    user: "root",
-    password: "Safa@123",
-    database: "foodmenu"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 
 });
 
 db.connect((err) => {
 
     if(err){
+
+        console.log("Database Connection Failed");
+
         console.log(err);
+
     }
 
     else{
+
         console.log("MySQL Connected");
+
     }
 
 });
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 
@@ -8,7 +10,9 @@ const app = express();
 
 /* MIDDLEWARE */
 
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 
 app.use(express.json());
 
